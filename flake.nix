0f83821330101d102e1b90864ab376434ec0689a
Adam Stephens <adam@valkor.net>
2023-07-24 14:22:26 -0400

    refactor for update script

--- flake.nix
+++ flake.nix
@@ -4,38 +4,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = [
+            pkgs.alejandra
+          ];
+        };
+
         packages = let
-          version = "0.2.0";
-
-          sf-pro-src = {
-            url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-            hash = "sha256-WG0nLn/Giiv0DT8zUwTiWuv/I23RqMSxJsGUbrQzCqc=";
-          };
-          sf-compact-src = {
-            url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-            hash = "sha256-uMGSFvqAfTdUWhNE6D6RyLKCrt4VXrUNZppvTHM7Igg=";
-          };
-          sf-mono-src = {
-            url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-            hash = "sha256-pqkYgJZttKKHqTYobBUjud0fW79dS5tdzYJ23we9TW4=";
-          };
-          sf-arabic-src = {
-            url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
-            hash = "sha256-V5JgeM13NUMnRFa0Xb90eo3jAq3hYYsek+1gCiIfFF4=";
-          };
-          ny-src = {
-            url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-            hash = "sha256-XOiWc4c7Yah+mM7axk8g1gY12vXamQF78Keqd3/0/cE=";
-          };
-
-          makeAppleFont = pname: pkgName: src:
+          makeAppleFont = name: pkgName: src:
             pkgs.stdenv.mkDerivation {
-              inherit pname src version;
+              inherit name src;
+
+              version = "0.3.0";
 
               unpackPhase = ''
                 undmg $src
@@ -43,7 +33,7 @@
                 7z x 'Payload~'
               '';
 
-              buildInputs = [pkgs.p7zip pkgs.undmg];
+              buildInputs = [pkgs.undmg pkgs.p7zip];
               setSourceRoot = "sourceRoot=`pwd`";
 
               installPhase = ''
@@ -54,12 +44,14 @@
                 find -name \*.ttf -exec mv {} $out/share/fonts/truetype/ \;
               '';
             };
+
+          sources = import ./sources.nix;
         in {
-          sf-pro = makeAppleFont "sf-pro" "SF Pro Fonts.pkg" (pkgs.fetchurl sf-pro-src);
-          sf-compact = makeAppleFont "sf-compact" "SF Compact Fonts.pkg" (pkgs.fetchurl sf-compact-src);
-          sf-mono = makeAppleFont "sf-mono" "SF Mono Fonts.pkg" (pkgs.fetchurl sf-mono-src);
-          sf-arabic = makeAppleFont "sf-arabic" "SF Arabic Fonts.pkg" (pkgs.fetchurl sf-arabic-src);
-          ny = makeAppleFont "ny" "NY Fonts.pkg" (pkgs.fetchurl ny-src);
+          sf-pro = makeAppleFont "sf-pro" "SF Pro Fonts.pkg" (pkgs.fetchurl sources.sf-pro);
+          sf-compact = makeAppleFont "sf-compact" "SF Compact Fonts.pkg" (pkgs.fetchurl sources.sf-compact);
+          sf-mono = makeAppleFont "sf-mono" "SF Mono Fonts.pkg" (pkgs.fetchurl sources.sf-mono);
+          sf-arabic = makeAppleFont "sf-arabic" "SF Arabic Fonts.pkg" (pkgs.fetchurl sources.sf-arabic);
+          ny = makeAppleFont "ny" "NY Fonts.pkg" (pkgs.fetchurl sources.ny);
         };
       };
     };
---
+++ sources.nix
@@ -0,0 +1,22 @@
+{
+  sf-pro = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
+    hash = "sha256-XoTegyl5BTBPHrKfaxJ18U2mzzxqCXLS9yUtN0hcB7I=";
+  };
+  sf-compact = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
+    hash = "sha256-7mk4i36CWPy08RdNTuFyahL3gb6HL7wwjWS9Zs1LH6s=";
+  };
+  sf-mono = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
+    hash = "sha256-pqkYgJZttKKHqTYobBUjud0fW79dS5tdzYJ23we9TW4=";
+  };
+  sf-arabic = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
+    hash = "sha256-MKcrCBtrxjm1DUZuvf2NKzcAzaiBjw1KgoDbKphrYkc=";
+  };
+  ny = {
+    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
+    hash = "sha256-XOiWc4c7Yah+mM7axk8g1gY12vXamQF78Keqd3/0/cE=";
+  };
+}
