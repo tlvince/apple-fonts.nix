94d3ca1c45cd838ef46f7137bb8afabc70a41cb6
Tom Vincent <git@tlvince.com>
2025-05-18 12:14:20 +0100

    chore(flake.lock): update

--- sources.nix
+++ sources.nix
@@ -1,22 +1,22 @@
 {
   sf-pro = {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    hash = "sha256-B8xljBAqOoRFXvSOkOKDDWeYUebtMmQLJ8lF05iFnXk=";
+    hash = "sha256-IccB0uWWfPCidHYX6sAusuEZX906dVYo8IaqeX7/O88=";
   };
   sf-compact = {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-    hash = "sha256-L4oLQ34Epw1/wLehU9sXQwUe/LaeKjHRxQAF6u2pfTo=";
+    hash = "sha256-PlraM6SwH8sTxnVBo6Lqt9B6tAZDC//VCPwr/PNcnlk=";
   };
   sf-mono = {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-    hash = "sha256-Uarx1TKO7g5yVBXAx6Yki065rz/wRuYiHPzzi6cTTl8=";
+    hash = "sha256-bUoLeOOqzQb5E/ZCzq0cfbSvNO1IhW1xcaLgtV2aeUU=";
   };
   sf-arabic = {
     url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
-    hash = "sha256-1clBp+aePSLNR9JrS+jReH7pEJtsH+zpzsiBKLQvvUs=";
+    hash = "sha256-J2DGLVArdwEsSVF8LqOS7C1MZH/gYJhckn30jRBRl7k=";
   };
   ny = {
     url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-    hash = "sha256-yYyqkox2x9nQ842laXCqA3UwOpUGyIfUuprX975OsLA=";
+    hash = "sha256-HC7ttFJswPMm+Lfql49aQzdWR2osjFYHJTdgjtuI+PQ=";
   };
 }
